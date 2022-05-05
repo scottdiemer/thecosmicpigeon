730db2511951114e1c90fdb4be07482cfe045fbd
scottdiemer <sdiemer@cfl.rr.com>
2022-05-05 12:59:18 -0400

    Working on footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,15 @@
 import Copyright from "./Copyright";
+import { LinkList } from "./LinkList";
+import links from "../data/links";
 
 export default function Footer() {
-  const listStyles = "";
+  const listStyles = "uppercase py-4";
   return (
-    <div className="bg-tertiary">
-      <ul></ul>
-      <Copyright name="The Cosmic Pigeon" />
+    <div className="bg-secondary/50 text-tertiary">
+      <ul className="flex justify-between container">
+        <LinkList links={links} className={listStyles} />
+      </ul>
+      <Copyright className="py-4" name="The Cosmic Pigeon" />
     </div>
   );
 }
